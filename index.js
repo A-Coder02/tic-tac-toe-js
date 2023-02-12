b252c4045b724b3459e04b83f7d4e3d47e045fbc
Arbaj Ansari <arbaj897ansari@gmail.com>
2023-02-12 22:46:08 +0530

    js basic setup done

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const aiPlayer = "X";
 const WIN_COMBOS = [
   [0, 1, 2],
   [3, 4, 5],
-  [6, 8, 8],
+  [6, 7, 8],
   [0, 3, 6],
-  [1, 4, 8],
+  [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6],
@@ -24,9 +24,9 @@ startGame();
 function startGame() {
   // document.querySelector(".endgame").style.display = "block";
   origBoard = Array.from(new Array(9).keys());
-  console.log(origBoard);
+  // console.log(origBoard);
   for (let i = 0; i < cells.length; i++) {
-    cells[i].innerHTML = "-";
+    cells[i].innerHTML = " ";
     cells[i].style.removeProperty("background-color");
     cells[i].addEventListener("click", turnClick, { once: true });
   }
@@ -37,8 +37,34 @@ function turnClick(square) {
 }
 
 function turn(squareId, player) {
-  console.log(squareId, player);
   origBoard[squareId] = player;
-  console.log(document.getElementById(squareId));
   document.getElementById(squareId).innerHTML = player;
+
+  let gameWon = checkWin(origBoard, player);
+  console.log(gameWon);
+  if (gameWon) gameOver(gameWon);
+}
+
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+  let gameWon = null;
+
+  for (let [index, win] of WIN_COMBOS.entries()) {
+    const isTrue = win.every((elt) => plays.includes(elt));
+    if (isTrue) {
+      gameWon = { index, player };
+    }
+  }
+  return gameWon;
+}
+
+function gameOver(gameWon) {
+  for (let index of WIN_COMBOS[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+      gameWon.player == humanPlayer ? "blue" : "red";
+  }
+
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", turnClick);
+  }
 }
